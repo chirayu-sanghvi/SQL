4e95d576815c619e97024ba97f30730266189fc9
chirayu-sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2023-12-27 22:05:57 -0500

    Create Weather observation 7.sql

---
+++ Easy/Weather observation 7.sql
@@ -0,0 +1,9 @@
+City ending with vowels
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+
+select distinct CITY from STATION where CITY like '%a' or CITY like '%e' or CITY like '%i' or CITY like '%o' or CITY like '%u' order by CITY;
