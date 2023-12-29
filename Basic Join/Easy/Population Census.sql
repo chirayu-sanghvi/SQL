5a0681833fef6ae0e2fed9f8a589e38ca138b343
chirayu-sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2023-12-28 22:02:25 -0500

    Update Population Census.sql

--- Basic Join/Easy/Population Census.sql
+++ Basic Join/Easy/Population Census.sql
@@ -19,5 +19,13 @@ Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 Solution:
 
 select city.name from city join country on city.countrycode = country.code where country.continent = 'Africa';
+======================================================================================================
+3) Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+Solution:
+
+select (country.continent) , floor(avg(city.population)) from city join country on city.countrycode = country.code group by country.continent;
 
   
