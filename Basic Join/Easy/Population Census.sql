694414593fa5a444b68bd04cc9408b6d59213a1b
chirayu-sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2023-12-28 21:55:24 -0500

    Update Population Census.sql

--- Basic Join/Easy/Population Census.sql
+++ Basic Join/Easy/Population Census.sql
@@ -9,3 +9,15 @@ Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 Solution:
 
 select sum(city.population) from city join country on city.countrycode = country.code where country.continent = 'Asia';
+
+
+======================================================================================================
+2) Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+Solution:
+
+select city.name from city join country on city.countrycode = country.code where country.continent = 'Africa';
+
+  
