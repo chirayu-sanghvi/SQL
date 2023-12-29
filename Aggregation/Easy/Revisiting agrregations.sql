c27cd4a601acac6cb497b27267f0b75b8f8f566c
chirayu-sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2023-12-28 20:42:09 -0500

    Update Revisiting agrregations.sql

--- Aggregation/Easy/Revisiting agrregations.sql
+++ Aggregation/Easy/Revisiting agrregations.sql
@@ -1,1 +1,20 @@
 
+
+1) Query a count of the number of cities in CITY having a Population larger than .
+  
+select count(*) from city 
+where population > 100000;
+==================================
+2) Query the total population of all cities in CITY where District is California.
+
+select Sum(POPULATION) as TOTAL_POPULATION from city where district = 'California';
+====================================
+3) Query the average population of all cities in CITY where District is California.
+
+select AVG(POPULATION) from city where district = 'California';
+=======================================
+4) Query the average population for all cities in CITY, rounded down to the nearest integer
+
+select FLOOR(AVG(POPULATION)) from city;
+========================================
+5)
