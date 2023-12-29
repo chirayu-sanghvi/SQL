60ae9d619e43e087ae208050f781c90e42a8b12d
chirayu-sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2023-12-28 21:52:45 -0500

    Update Population Census.sql

--- Basic Join/Easy/Population Census.sql
+++ Basic Join/Easy/Population Census.sql
@@ -1,1 +1,11 @@
+Question Link: https://www.hackerrank.com/challenges/asian-population/problem
 
+prompt: Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+  ===================
+
+Solution:
+
+select sum(city.population) from city join country on city.countrycode = country.code where country.continent = 'Asia';
