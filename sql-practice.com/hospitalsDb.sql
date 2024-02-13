c76692747709e4ddc49b72b506d39e2d1c565c0b
Chirayu Sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2024-02-13 16:37:06 -0500

    Create hospitalsDb.sql

---
+++ sql-practice.com/hospitalsDb.sql
@@ -0,0 +1,21 @@
+PATIENT TABLE SCHEMA
+
+primary key icon	patient_id	INT
+first_name	TEXT
+last_name	TEXT
+gender	CHAR(1)
+birth_date	DATE
+city	TEXT
+primary key icon	province_id	CHAR(2)
+allergies	TEXT
+height	INT
+weight	INT
+
+
+*/
+Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
+*/
+
+UPDATE patients
+SET allergies = 'NKA'
+WHERE allergies IS NULL;
