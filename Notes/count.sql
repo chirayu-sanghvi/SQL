364cc29a0b9437304356ad048bab8ff83156f379
Chirayu Sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2024-02-13 21:31:39 -0500

    Create count.sql

---
+++ Notes/count.sql
@@ -0,0 +1,11 @@
+/*
+SQL COUNT() Function
+The COUNT() function returns the number of rows that matches a specified criterion.
+
+COUNT() usually has * as it's parameter because the name of the column does not usually matter since they all have the same count.
+
+COUNT() Syntax
+*/
+SELECT COUNT(column_name)
+FROM table_name
+WHERE condition;
